5bbee9ff91bb03351f45288da69133b3dfe5b9eb
BINDHUMADHAVAVARMA <54420831+BINDHUMADHAVAVARMA@users.noreply.github.com>
2019-09-12 15:34:03 +0530

    Add files via upload

---
+++ Prime_Factors.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Prime_Factors{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int n,i,j;
+		n=sc.nextInt();
+		for(i=2;i<=n;i++){
+            for(j=1;j<=i;j++){ 
+			   if(i%j==0){if(n%i==0){n=n/i;System.out.print(i +" ");}}
+
+			}   	
+		}
+
+	}
+} 
